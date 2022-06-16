36e7dca4a0b8f2d35fdab1a30368d906ab9a008c
qijiefeng <qijiefeng@9bxkj.onaliyun.com>
2022-06-16 08:33:19 +0800

    温度数据接收

--- src/pages/temperaturePage/components/measurement.jsx
+++ src/pages/temperaturePage/components/measurement.jsx
@@ -2,14 +2,29 @@ import React, {
     useEffect,
     useState,
 } from 'react';
-import { Button, Progress } from 'antd';
+import { Button, Progress, Layout } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import Animation_1 from './../../../assets/img/Animation_1.png';
 import Animation_2 from './../../../assets/img/Animation_2.png';
 import Animation_3 from './../../../assets/img/Animation_3.png';
+import HeaderItem from './headerItem';
+import { connect } from 'react-redux';
+import {
+    selectHardwareModalShowFun,
+    petSortTypeFun,
+    petDetailInfoFun,
+    setMellaConnectStatusFun,
+    setMellaMeasureValueFun,
+    setMellaPredictValueFun,
+    setMellaMeasurePartFun
+} from '../../../store/actions';
+import _ from 'lodash';
 import './measurement.less';
 
-const Measurement = () => {
+const { Content, Header } = Layout;
+
+const Measurement = ({ petMessage, hardwareMessage }) => {
+    let { mellaMeasureValue } = hardwareMessage;
     const [percent, setPercent] = useState(0);
     const decline = () => {
         let newPercent = percent + 10;
@@ -18,16 +33,39 @@ const Measurement = () => {
         }
         setPercent(newPercent);
     }
+    //圆滑里面的文字
+    const ProgressTitle = (percent) => {
+        return (
+            <p className='ProgressTitle'>{percent} ℃</p>
+        );
+    }
     return (
-        <div className='measurementBox'>
-            <Progress type="dashboard" percent={percent} gapDegree={30} width={'260px'} strokeWidth={'8'} format={percent => `${percent} ℉`} />
-            <Button onClick={decline} icon={<PlusOutlined />} />
-            {
-                percent === 0 ? (<img src={Animation_1} />) : (<img src={Animation_2} />)
-            }
+        <>
+            <HeaderItem />
+            <Content className={"contentBox"}>
+                <Progress type="dashboard" percent={_.round(mellaMeasureValue, 1)} gapDegree={30} width={'260px'} strokeWidth={'8'} format={percent => ProgressTitle(percent)} />
+                <Button onClick={decline} icon={<PlusOutlined />} />
+                {
+                    percent === 0 ? (<img src={Animation_1} />) : (<img src={Animation_2} />)
+                }
+            </Content>
+        </>
 
-        </div>
     );
 };
 
-export default Measurement;
+export default connect(
+    state => ({
+        petMessage: state.petReduce.petDetailInfo,
+        hardwareMessage: state.hardwareReduce,
+    }),
+    {
+        selectHardwareModalShowFun,
+        petSortTypeFun,
+        petDetailInfoFun,
+        setMellaConnectStatusFun,
+        setMellaMeasureValueFun,
+        setMellaPredictValueFun,
+        setMellaMeasurePartFun
+    }
+)(Measurement);
--- src/pages/temperaturePage/index.jsx
+++ src/pages/temperaturePage/index.jsx
@@ -7,34 +7,68 @@ import { Layout, Menu, PageHeader } from 'antd';
 import LinkEquipment from './components/linkEquipment';
 import Measurement from './components/measurement';
 import MeasuredData from './components/measuredData';
-import { selectHardwareModalShowFun, petSortTypeFun, petDetailInfoFun } from '../../store/actions';
+import {
+    selectHardwareModalShowFun,
+    petSortTypeFun,
+    petDetailInfoFun,
+    setMellaConnectStatusFun,
+    setMellaMeasureValueFun,
+    setMellaPredictValueFun,
+    setMellaMeasurePartFun
+} from '../../store/actions';
 import _ from 'lodash';
 import './index.less';
 
 const { Content, Header } = Layout;
 
-const TemperaturePage = ({ petMessage }) => {
+const TemperaturePage = ({ petMessage, hardwareMessage }) => {
+    let { mellaConnectStatus } = hardwareMessage;
+    //测量温度中的页面变化
+    const changePage = () => {
+        switch (mellaConnectStatus) {
+            case 'isMeasuring':
+                return <Measurement />
+            case 'complete':
+                return <MeasuredData />
+            case 'connected':
+                return <LinkEquipment />
+            case 'disconnected':
+                return <LinkEquipment />
+            default:
+                break;
+        }
 
+    }
+    useEffect(() => {
+
+        return (() => { })
+    }, [mellaConnectStatus])
     return (
         <Layout className='homeBox'>
             {
-                _.isEmpty(petMessage) ? (
-                    <div className='chackPatientBox'>
-                        <p className='chackPatientTitle'>Select a patient</p>
-                    </div>
-                ) : (
-                    <LinkEquipment />
-                )
+                _.isEmpty(petMessage) ?
+                    (
+                        <div className='chackPatientBox'>
+                            <p className='chackPatientTitle'>Select a patient</p>
+                        </div>
+                    ) : (changePage())
             }
-            {/* <Measurement/> */}
-            {/* <MeasuredData/> */}
         </Layout>
     );
 };
 
 export default connect(
     state => ({
-        petMessage: state.petReduce.petDetailInfo
+        petMessage: state.petReduce.petDetailInfo,
+        hardwareMessage: state.hardwareReduce,
     }),
-    { selectHardwareModalShowFun, petSortTypeFun, petDetailInfoFun }
+    {
+        selectHardwareModalShowFun,
+        petSortTypeFun,
+        petDetailInfoFun,
+        setMellaConnectStatusFun,
+        setMellaMeasureValueFun,
+        setMellaPredictValueFun,
+        setMellaMeasurePartFun
+    }
 )(TemperaturePage);
