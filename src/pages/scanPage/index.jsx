b00722dc187a990d23e498d67dee931cb0e762ea
qijiefeng <qijiefeng@9bxkj.onaliyun.com>
2022-06-20 19:09:28 +0800

    修改温度

--- src/pages/scanPage/index.jsx
+++ src/pages/scanPage/index.jsx
@@ -1,50 +1,46 @@
-import React, {
-    useEffect,
-    useState,
-} from 'react';
-import { connect } from 'react-redux';
-import { Layout, Menu, PageHeader } from 'antd';
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { Layout, Menu, PageHeader } from "antd";
 import {
+  selectHardwareModalShowFun,
+  petSortTypeFun,
+  petDetailInfoFun,
+  setMellaConnectStatusFun,
+  setMellaMeasureValueFun,
+  setMellaPredictValueFun,
+  setMellaMeasurePartFun,
+} from "../../store/actions";
+import _ from "lodash";
+import HeaderItem from "../temperaturePage/components/headerItem";
+import LinkEquipment from "./components/linkEquipment";
+import ScanPet from "./components/scanPet";
+import "./index.less";
+
+const ScanPage = ({ petMessage, hardwareMessage }) => {
+  let { petId } = petMessage;
+
+  return (
+    <>
+      <Layout className="homeBox">
+        <HeaderItem />
+        {_.isEmpty(petId) ? <LinkEquipment /> : <ScanPet />}
+      </Layout>
+    </>
+  );
+};
+
+export default connect(
+  (state) => ({
+    petMessage: state.petReduce.petDetailInfo,
+    hardwareMessage: state.hardwareReduce,
+  }),
+  {
     selectHardwareModalShowFun,
     petSortTypeFun,
     petDetailInfoFun,
     setMellaConnectStatusFun,
     setMellaMeasureValueFun,
     setMellaPredictValueFun,
-    setMellaMeasurePartFun
-} from '../../store/actions';
-import _ from 'lodash';
-import HeaderItem from '../temperaturePage/components/headerItem';
-import LinkEquipment from './components/linkEquipment';
-import ScanPet from './components/scanPet';
-import './index.less';
-
-const ScanPage = ({ petMessage, hardwareMessage }) => {
-    console.log('petMessage', petMessage);
-    let { mellaConnectStatus } = hardwareMessage;
-    return (
-        <>
-            <Layout className='homeBox'>
-                <HeaderItem />
-                {/* <LinkEquipment/> */}
-                <ScanPet />
-            </Layout>
-        </>
-    );
-};
-
-export default connect(
-    state => ({
-        petMessage: state.petReduce.petDetailInfo,
-        hardwareMessage: state.hardwareReduce,
-    }),
-    {
-        selectHardwareModalShowFun,
-        petSortTypeFun,
-        petDetailInfoFun,
-        setMellaConnectStatusFun,
-        setMellaMeasureValueFun,
-        setMellaPredictValueFun,
-        setMellaMeasurePartFun
-    }
+    setMellaMeasurePartFun,
+  }
 )(ScanPage);
